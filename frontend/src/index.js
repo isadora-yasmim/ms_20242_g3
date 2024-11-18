23772acae78f9bddcfba5d86b427677a3f9d00e0
Isadora Yasmim <164579225+isadora-yasmim@users.noreply.github.com>
2024-11-18 20:10:22 -0300

    Revert "correção de erros"
    This reverts commit a2a3bbd1ca9e23dfb69f497bb1f7d456f820b4e5.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.tsx';
-import HomePage from './Homepage';
+import './similarFilmspage.css';
+import SimilarFilmsPage from './similarFilmspage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HomePage />
+    <SimilarFilmsPage />
   </React.StrictMode>
 );
 
